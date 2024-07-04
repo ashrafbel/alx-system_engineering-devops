394e633ca3439085f86c79b03b2360050030a27b
ashrafbel <ashrafmus2@gmail.com>
2024-07-03 17:38:31 -0700

    2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,5 @@
 # 2-execute_a_command.pp
-
-exec { 'kill_killmenow_process':
-  command     => '/usr/bin/pkill -f killmenow || /bin/true',
-  path        => ['/usr/bin', '/bin', '/usr/sbin', '/sbin'],
-  onlyif      => 'pgrep -f killmenow',
-  refreshonly => true,
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
 }
