56581677eb286d24ee68672a8091f564dbed5d0b
ashrafbel <ashrafmus2@gmail.com>
2024-07-29 10:47:08 -0700

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""this script to fetch TODO list progress
-for a given employee ID using REST API.
-"""
+"""This script fetches TODO list progress for a given employee ID using REST API."""
 
 import requests
 import sys
@@ -9,28 +7,32 @@ import sys
 URL_USERS_API = "https://jsonplaceholder.typicode.com/users/"
 URL_TODOS_API = "https://jsonplaceholder.typicode.com/todos?userId="
 
-if len(sys.argv) == 2:
-    try:
-        id_emp = int(sys.argv[1])
-        url_users = f"{URL_USERS_API}{id_emp}"
-        url_todos = f"{URL_TODOS_API}{id_emp}"
-
-        resp_user = requests.get(url_users)
-        data_user = resp_user.json()
-        name_of_user = data_user.get('name')
-        resp_todos = requests.get(url_todos)
-        data_todos = resp_todos.json()
-        tasks = len(data_todos)
-        done = [t for t in data_todos if t.get('completed')]
-        num_of_tasks_done = len(done)
-        
-        print(f"Employee {name_of_user} is done with tasks({num_of_tasks_done}/{tasks}):")
-        for t in done:
-            print("\t{}".format(t.get('title')))
-            
-    except ValueError:
-        print("EMPLOYEE_ID must be an integer")
-    except requests.exceptions.RequestException as e:
-        print(f"HTTP Request failed: {e}")
-else:
+if len(sys.argv) != 2:
     print("Usage: python script.py EMPLOYEE_ID")
+    sys.exit(1)
+
+try:
+    id_emp = int(sys.argv[1])
+    url_users = f"{URL_USERS_API}{id_emp}"
+    url_todos = f"{URL_TODOS_API}{id_emp}"
+
+    resp_user = requests.get(url_users)
+    data_user = resp_user.json()
+    name_of_user = data_user.get('name')
+
+    resp_todos = requests.get(url_todos)
+    data_todos = resp_todos.json()
+    
+    tasks = len(data_todos)
+    done = [t for t in data_todos if t.get('completed')]
+    num_of_tasks_done = len(done)
+
+    print(f"Employee {name_of_user} is done with tasks({num_of_tasks_done}/{tasks}):")
+    for t in done:
+        print("\t {}".format(t.get('title')))
+
+except ValueError:
+    print("EMPLOYEE_ID must be an integer")
+except requests.exceptions.RequestException as e:
+    print(f"HTTP Request failed: {e}")
+
