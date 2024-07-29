3ef6ce6e40fab084fa06246b2c2778d587adf261
ashrafbel <ashrafmus2@gmail.com>
2024-07-29 14:18:17 -0700

    3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python3
-"""This script fetches TODO list progress for all employees using REST API
-and exports the data in JSON format.
+"""this script to fetch TODO list progress
+for a given employee ID using REST API.
 """
 import json
 import requests
+
 
 URL_USERS_API = "https://jsonplaceholder.typicode.com/users"
 URL_TODOS_API = "https://jsonplaceholder.typicode.com/todos"
 
 try:
-    # جلب بيانات جميع المستخدمين
     resp_users = requests.get(URL_USERS_API)
     resp_users.raise_for_status()
     users_data = resp_users.json()
 
-    # جلب بيانات جميع المهام
     resp_todos = requests.get(URL_TODOS_API)
     resp_todos.raise_for_status()
     todos_data = resp_todos.json()
 
     all_data = {}
-
-    # معالجة البيانات
     for user in users_data:
         user_id = user['id']
         username = user['username']
@@ -37,12 +34,9 @@ try:
                 user_tasks.append(task_info)
 
         all_data[str(user_id)] = user_tasks
-
-    # إنشاء ملف JSON
-    file_name = 'todo_all_employees.json'
-    with open(file_name, mode='w') as file:
-        json.dump(all_data, file, indent=4)
-
+    fl_name = 'todo_all_employees.json'
+    with open(fl_name, mode='w') as fl:
+        json.dump(all_data, fl)
 
 except requests.exceptions.RequestException as e:
     print(f"HTTP Request failed: {e}")
