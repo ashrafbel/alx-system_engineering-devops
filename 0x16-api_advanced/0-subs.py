630fc1a713e635ca388b4e1ba9995293768d9481
ashrafbel <ashrafmus2@gmail.com>
2024-08-13 15:13:15 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-"dfdfsdffs"
+"Module"
 import requests
 
+
 def number_of_subscribers(subreddit):
-    "sddsfsfsdf"
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    "queries the Reddit API to return the number of subscribers"
+    u = f"https://www.reddit.com/r/{subreddit}/about"
+
+
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+            "User-Agent": "CustomClient/1.0"
     }
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        try:
-            data = response.json()
-            return data['data']['subscribers']
-        except (KeyError, ValueError):
-            return 0
+    res_ = requests.get(u, headers=headers, allow_redirects=False)
+    if res_.status_code == 200:
+        info_ = res_.json().get('data').get('subreddit')
+        return info_
     else:
         return 0
