f610eea70547350bc3350970f149708b0692ba97
ashrafbel <ashrafmus2@gmail.com>
2024-08-13 18:10:00 -0700

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,15 +3,14 @@
 
 import requests
 
+
 def top_ten(subreddit):
     "Queries Reddit API to print the first 10 hot post titles for a subreddit"
     u = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {
         "User-Agent": "Custom"
     }
-    
     res_ = requests.get(u, headers=headers, allow_redirects=False)
-    
     if res_.status_code == 200:
         Info_ = res_ .json().get('data')
         if Info_:
