33973f4558c99857e1d04cd2a107bcba539d40e1
ashrafbel <ashrafmus2@gmail.com>
2024-08-14 19:57:47 -0700

    0-strace_is_your_friend.pp

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Puppet manifest to fix 'phpp' extensions in WordPress configuration
+
+exec { 'fix_phpp':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
+}
