764da37ca8450f701c008c2035f35595366c600b
ashrafbel <ashrafmus2@gmail.com>
2024-08-15 19:19:50 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,16 +2,18 @@
 "Module"
 import requests
 
-
 def number_of_subscribers(subreddit):
-    "queries the Reddit API to return the number of subscribers"
-    u = f"https://www.reddit.com/r/{subreddit}/about"
+    """
+    Queries the Reddit API and returns the number of subscribers
+    for the given subreddit.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
-            "User-Agent": "CustomClient/1.0"
+        "User-Agent": "CustomClient/1.0"
     }
-    res_ = requests.get(u, headers=headers, allow_redirects=False)
+    res_ = requests.get(url, headers=headers, allow_redirects=False)
     if res_.status_code == 200:
-        info_ = res_.json().get('data').get('subreddit')
+        info_ = res_.json().get('data').get('subscribers')
         return info_
     else:
         return 0
