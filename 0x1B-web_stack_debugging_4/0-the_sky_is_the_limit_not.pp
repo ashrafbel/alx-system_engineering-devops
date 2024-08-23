32c5ec5aae8c4dd5f3c3243a63ca4e811cd85a3b
ashrafbel <ashrafmus2@gmail.com>
2024-08-22 19:22:50 -0700

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # Increases the capacity of an Nginx server to handle higher traffic volumes
 
-exec { 'nginx fix':
+exec { 'nginx-fix':
   onlyif   => 'test -e /etc/default/nginx',
   command  => "sed -i s/'-n 15'/'-n 4096'/g /etc/default/nginx;  service nginx restart",
   provider => 'shell'
