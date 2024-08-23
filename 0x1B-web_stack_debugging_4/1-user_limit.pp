201194affc5865d95f32cbf54588a12c42badfb3
ashrafbel <ashrafmus2@gmail.com>
2024-08-22 19:21:29 -0700

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,11 @@
-# Enable the user holberton to login and open files without error.
+# Puppet script to increase the open file limit for the holberton user
 
-# Increase hard file limit for Holberton user.
-exec { 'increase-hard-file-limit-for-holberton-user':
+exec { 'increase_nofile_soft_limit':
   command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
 
-# Increase soft file limit for Holberton user.
-exec { 'increase-soft-file-limit-for-holberton-user':
+exec { 'increase_nofile_hard_limit':
   command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
